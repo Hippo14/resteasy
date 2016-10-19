71887ff7e4eedf39badf1e8307a2e3fc2ed2ab2f
Hippo14 <hippo14@gmail.com>
2016-10-19 12:06:32 +0100

    Added filter auth

---
+++ src/main/java/filter/AuthFilter.java
@@ -0,0 +1,83 @@
+package filter;
+
+import org.apache.commons.codec.language.Soundex;
+import org.apache.commons.io.IOUtils;
+import org.apache.log4j.Logger;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+import java.io.*;
+
+/**
+ * Created by MSI on 2016-10-19.
+ */
+public class AuthFilter implements Filter {
+
+    final static Logger LOG = Logger.getLogger(AuthFilter.class);
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        String json = servletRequest.getParameter("token");
+        LOG.info("TOKEN REQUEST: " + json);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+    private static class ResettableStreamHttpServletRequest extends
+            HttpServletRequestWrapper {
+
+        private byte[] rawData;
+        private HttpServletRequest request;
+        private ResettableServletInputStream servletStream;
+
+        public ResettableStreamHttpServletRequest(HttpServletRequest request) {
+            super(request);
+            this.request = request;
+            this.servletStream = new ResettableServletInputStream();
+        }
+
+
+        public void resetInputStream() {
+            servletStream.stream = new ByteArrayInputStream(rawData);
+        }
+
+        @Override
+        public ServletInputStream getInputStream() throws IOException {
+            if (rawData == null) {
+                rawData = IOUtils.toByteArray(this.request.getReader());
+                servletStream.stream = new ByteArrayInputStream(rawData);
+            }
+            return servletStream;
+        }
+
+        @Override
+        public BufferedReader getReader() throws IOException {
+            if (rawData == null) {
+                rawData = IOUtils.toByteArray(this.request.getReader());
+                servletStream.stream = new ByteArrayInputStream(rawData);
+            }
+            return new BufferedReader(new InputStreamReader(servletStream));
+        }
+
+
+        private class ResettableServletInputStream extends ServletInputStream {
+
+            private InputStream stream;
+
+            @Override
+            public int read() throws IOException {
+                return stream.read();
+            }
+        }
+    }
+
+}
