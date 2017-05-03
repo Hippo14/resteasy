1aef0de41bcdb0261be940f71a54462f8dcfdb7f
Hippo14 <hippo14@gmail.com>
2017-05-03 17:18:12 +0200

    Testing configuration bean

---
+++ src/main/java/conf/ConfigurationBean.java
@@ -0,0 +1,44 @@
+package conf;
+
+
+import dao.UsersDAO;
+import org.apache.log4j.Logger;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.ejb.Schedule;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+
+/**
+ * Created by MSI on 2016-10-19.
+ */
+@Startup
+@Singleton
+public class ConfigurationBean {
+
+    public enum States {BEFORESTARTED, STARTED, PAUSED, SHUTTINGDOWN};
+    private States state;
+    final static Logger LOG = Logger.getLogger(UsersDAO.class);
+
+    @PostConstruct
+    public void initialize() {
+        state = States.BEFORESTARTED;
+        // Perform initialization
+        state = States.STARTED;
+        LOG.info("Service Started");
+    }
+
+    @PreDestroy
+    public void terminate() {
+        state = States.SHUTTINGDOWN;
+        // Permform termination
+        LOG.info("Shut down in progress");
+    }
+
+    @Schedule(second = "*", minute = "*/5", hour = "*", persistent = false)
+    public void doWork() {
+        System.out.println("Do work!");
+    }
+
+}
